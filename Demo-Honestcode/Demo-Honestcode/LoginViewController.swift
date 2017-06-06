1b90f6e753852b9017a893458ec8175249b33a83
Jose Angel Cuadrado Mingo <joseangel.cuadrado@intelygenz.com>
2017-06-06 16:09:47 +0200

    Create controllers associated to views

---
+++ Demo-Honestcode/Demo-Honestcode/LoginViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LoginViewController.swift
+//  Demo-Honestcode
+//
+//  Created by Jose Angel  Cuadrado Mingo on 6/6/17.
+//  Copyright © 2017 HonestCode. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
---
+++ Demo-Honestcode/Demo-Honestcode/SignUpViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SignUpViewController.swift
+//  Demo-Honestcode
+//
+//  Created by Jose Angel  Cuadrado Mingo on 6/6/17.
+//  Copyright © 2017 HonestCode. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
